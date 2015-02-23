ef6c4000785224f7dbd839b22382cab494d4ab84
jjjjustin <justinsitarz@gmail.com>
2015-02-22 19:15:04 -0600

    fixed has attached settings

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,8 +5,14 @@ class Room < ActiveRecord::Base
   attr_accessor :poster_image_updated_at
 
   has_many :meetings
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image,
+                    :url =>':s3_domain_url',
+                    :path => '/:class/:attachment/:id_partition/:style/:filename',
+                    :styles => { :medium => "300x300>",
+                    :thumb => "100x100>" },
+                    :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 
 end
+
